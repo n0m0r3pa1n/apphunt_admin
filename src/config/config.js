e2963b56a7f3f36b36e9600a9ad2c03cca55a23e
Georgi Mirchev <gmirchev90@gmail.com>
2015-09-11 15:46:57 +0300

    AppHunt production server url

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@ export var FLURRY_BASE_URL = "http://api.flurry.com/"
 export var FLURRY_API_ACCESS_CODE = "K3DM6PJMTV58CRN4MK6Q"
 export var FLURRY_API_KEY = "TF65K4T659FTCPXGMNG3"
 
-export var BASE_URL = "https://apphunt-dev.herokuapp.com/"
+export var BASE_URL = "https://apphunt.herokuapp.com/"
 //export var BASE_URL = "http://localhost:8080/"
 export var APP_COMMENTS_URL = BASE_URL + "comments/"
 export var APP_COLLECTIONS_URL = BASE_URL + "app-collections"
